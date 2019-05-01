b7be4200f52cdec4daf7b213387810cfd39aa8a7
Devin Metivier <devinmetivier@gmail.com>
2019-04-30 21:33:58 -0400

    add proxy code and dark styles

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  const html = (await (await fetch(`https://github.com${req.url}`)).text())
+    .replace(/(href=.)https:?\/\/github.com/g, `$1//${req.headers.host}`)
+    .replace(
+      '</head>',
+      '<link media="all" href="/dark.css" rel="stylesheet" /></head>'
+    );
+
+  res.end(html);
+};
